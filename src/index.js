261fa7c9d3edc414c9439646b83f69e99c6c3006
nara-ryoya <nara_ryoya@icloud.com>
2021-03-11 17:28:43 +0900

    やっと治った

--- src/index.js
+++ src/index.js
@@ -91,7 +91,7 @@ class Game extends React.Component {
 
     const moves = history.map((step, move) => {
       const desc = move ?
-        'Go to move #' + move :
+        'Go to move #' + move:
         'Go to game start';
       return (
         <li key={move}>
